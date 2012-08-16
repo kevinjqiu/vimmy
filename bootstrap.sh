bdcd74a84492ce1d4091552516769c4dfa4b3ca2
Kevin Qiu <kevin@freshbooks.com>
2012-08-16 00:37:57 -0400

    Fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,11 +18,11 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
     [ -e $i ] && mv $i $i.$TODAY
 done
 
-git clone --recursive $REPO_URL $ENDPATH
+git clone --recursive -b vundle $REPO_URL $ENDPATH
 mkdir -p $ENDPATH/.vim/bundle
 ln -s $ENDPATH/vimrc $HOME/.vimrc
 ln -s $ENDPATH/vim $HOME/.vim
 
-git clone -b vundle http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
 vim +BundleInstall! +BundleClean +q
