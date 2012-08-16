389b4f9e247ee88ccefbfc8dc4b2953058474028
Kevin Qiu <kevin@freshbooks.com>
2012-08-16 00:20:04 -0400

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env sh
+
+ENDPATH="$HOME/vimmy"
+REPO_URL="http://github.com/kevinjqiu/vimmy.git"
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+TODAY=`date +%Y%m%d`
+
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
+    [ -e $i ] && mv $i $i.$TODAY
+done
+
+git clone --recursive $REPO_URL $ENDPATH
+mkdir -p $ENDPATH/.vim/bundle
+ln -s $ENDPATH/.vimrc $HOME/.vimrc
+ln -s $ENDPATH/.vim $HOME/.vim
+
+git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+
+vim +BundleInstall! +BundleClean +q
