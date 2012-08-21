93684d27248ff9dc7770caea2866bca4b1e5bfdb
Kevin Qiu <kevin@freshbooks.com>
2012-08-21 19:27:58 -0400

    updated script

--- update_snippets.sh
+++ update_snippets.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env sh
-
-git clone http://github.com/kevinjqiu/snipmate-snippets
+[ ! -e snipmate-snippets ] && git clone http://github.com/kevinjqiu/snipmate-snippets
 cd snipmate-snippets && rake deploy_local
